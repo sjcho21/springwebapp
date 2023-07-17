d3fc192b807d59c338b8bd3027758ef83e28b3f4
sjcho21 <sjcho21@gamil.com>
2023-07-17 15:40:28 +0900

    최초 프로젝트 공유 파일

---
+++ src/main/java/com/mycompany/springwebapp/controller/HomeController.java
@@ -0,0 +1,16 @@
+package com.mycompany.springwebapp.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@Controller
+public class HomeController {
+		//http://localhost:8080/springwebapp/
+		@RequestMapping("/")
+		public String index() {
+			return "index";
+		}
+}
---
+++ src/test/java/com/mycompant/springwebapp/Test.java
@@ -0,0 +1,5 @@
+package com.mycompant.springwebapp;
+
+public class Test {
+
+}
