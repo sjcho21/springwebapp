5a3532d5a319d60ba2015638a41ce75ccd8d61d9
sjcho21 <sjcho21@gamil.com>
2023-07-18 16:45:33 +0900

    웹 애플리케이션 설정

--- src/main/java/com/mycompany/springwebapp/controller/HomeController.java
+++ src/main/java/com/mycompany/springwebapp/controller/HomeController.java
@@ -6,10 +6,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
-@Controller
+@Controller //
 public class HomeController {
 		//http://localhost:8080/springwebapp/
-		@RequestMapping("/")
+		@RequestMapping("/") //conroller에 requestMapping 을 작성하면 anootation에서 정보를 보관해서 controller 에 쏴준다.
 		public String index() {
 			log.info("실행1");
 			return "index";
